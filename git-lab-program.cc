9b8a981241bcdb7078ca7778a49a6e52fe3655a5
caden <cr750623@ohio.edu>
2024-09-05 17:10:44 -0400

    program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Caden Rogers
+ *        Date:  9/5/2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
